d9b24fc378614223d767e21cc28191e5d0e08958
Meshu Deb Nath <meshu.uiu@gmail.com>
2020-03-05 02:14:54 +0100

    Updates

---
+++ precache-manifest.9d4271b36db31d0cb53a48e28fe6e489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80dfc148b675431d72ee1d65af0bb53",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "f9312b6b8e369c208d42",
+    "url": "/meme-generator/static/css/main.2b7f3370.chunk.css"
+  },
+  {
+    "revision": "b970ecf9807fa7dab767",
+    "url": "/meme-generator/static/js/2.264933b0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/meme-generator/static/js/2.264933b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9312b6b8e369c208d42",
+    "url": "/meme-generator/static/js/main.8e0d01be.chunk.js"
+  },
+  {
+    "revision": "cc48f9e75a7fed5be940",
+    "url": "/meme-generator/static/js/runtime-main.8af17244.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/meme-generator/static/media/Trollface.be324d5b.png"
+  }
+]);
